f2b23f687cca7129c5960d4aeb77a42a3f9edbab
batyrbek01 <batyr070605@gmail.com>
2022-08-29 15:09:32 +0300

    abc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
+    public static int bossHealth = 700;
+    public static int bossDamage = 50;
+
+    public static int[] heroesHealth = {270, 280, 260, 95,350};
+    public static int[] heroesDamage = {20, 15, 25, 20};
+
+    public static String[] heroesAttackType = {"Physical","Magical","Kinetic", "Medic","Golem"};
+
+    public static int roundNumber = 0;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printStatistics();
+    }
+    public static void printStatistics(){
+        System.out.println(" "+ roundNumber + " ROUND ");
+        System.out.println  ("Boss health: "+ bossHealth + " ["+ bossDamage +" ] ");
     }
 }
+
